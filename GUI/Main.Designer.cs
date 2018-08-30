ddf9ac36eed0edfddd6c29f109be28bf23b6c41e
HebaruSan <HebaruSan@users.noreply.github.com>
2018-08-29 21:59:56 -0500

    Allow smaller minimum size in GUI
    1280x700 is kind of gigantic

--- GUI/Main.Designer.cs
+++ GUI/Main.Designer.cs
@@ -1068,7 +1068,7 @@
             this.KeyPreview = true;
             this.MainMenuStrip = this.menuStrip1;
             this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.MinimumSize = new System.Drawing.Size(1280, 700);
+            this.MinimumSize = new System.Drawing.Size(880, 400);
             this.Name = "Main";
             this.Text = "CKAN-GUI";
             this.menuStrip1.ResumeLayout(false);
